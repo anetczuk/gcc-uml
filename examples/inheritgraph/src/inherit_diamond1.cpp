557a18914b86da37c1fbc1f5fe3d6b53fd5b1692
anetczuk <anetczuk@o2.pl>
2025-03-15 23:50:46 +0100

    Update inheritance diamond example

--- examples/inheritgraph/src/inherit_diamond1.cpp
+++ examples/inheritgraph/src/inherit_diamond1.cpp
@@ -1,35 +1,37 @@
 ///
 
 namespace property {
-	class Animal {
-	public:
-		int size;
-	};
+    class Animal {
+    public:
+        double weight;
+        double width;
+        double height;
+    };
 
 
-	class FlyingAnimal: public Animal {
-	public:
-		double flydistance = 0.0;
-		void fly(const double distance) {
-			flydistance += distance;
-		}
-	};
+    class FlyingAnimal: public Animal {
+    public:
+        double flydistance = 0.0;
+        void fly(const double distance) {
+            flydistance += distance;
+        }
+    };
 
 
-	class WalkingAnimal: public Animal {
-	public:
-		double walkdistance = 0.0;
-		void walk(const double distance) {
-			walkdistance += distance;
-		}
-	};
+    class WalkingAnimal: public Animal {
+    public:
+        double walkdistance = 0.0;
+        void walk(const double distance) {
+            walkdistance += distance;
+        }
+    };
 }
 
 
 class Duck: public property::FlyingAnimal, public property::WalkingAnimal {
 public:
-	int noquacks = 0;
-	void quack() {
-		++noquacks;
-	}
+    int noquacks = 0;
+    void quack() {
+        ++noquacks;
+    }
 };
